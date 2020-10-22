bec5d22f23bc43c486e0cd15a60f7300d6d6c6ff
jyoniki <72187229+jyoniki@users.noreply.github.com>
2020-10-22 10:25:24 +0900

    first commit

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,56 @@
+class PrototypesController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show, :new]
+
+  def index
+    @prototypes = Prototype.includes(:user)
+  end
+  
+  def new
+    @prototype = Prototype.new
+  end
+  
+  def create
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to root_path
+    else
+      render "prototypes/new"
+    end
+  end
+
+  def show
+    @prototype = Prototype.find(params[:id])
+    @comment = Comment.new
+    @comments = @prototype.comments.includes(:user)
+  end
+  
+  def edit
+    @prototype = Prototype.find(params[:id])
+  end
+  
+  def update
+    @prototype = Prototype.find(params[:id])
+    if @prototype.update(prototype_params)
+      redirect_to prototype_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    purototype = Prototype.find(params[:id])
+    purototype.destroy
+    redirect_to root_path
+  end
+  
+  private
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_sessions_new
+    unless user_signed_in?
+      redirect_to action: edit
+    end
+  end
+end
